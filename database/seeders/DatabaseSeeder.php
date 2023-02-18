f15b98a8f83f29830fa5d53bce33a9d9b43228ce
Xyber Pastoril <xyber.pastoril@proton.me>
2023-02-18 11:06:46 +0800

    Seed initial user accounts

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        // Create User Accounts for Admin and User
+        \App\Models\User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@pg.qrams.dev',
+            'is_admin' => true,
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'User',
+            'email' => 'user@pg.qrams.dev',
+        ]);
     }
 }
