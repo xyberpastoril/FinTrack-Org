49bb6c5597fbf2a256d5212b46db1d4a8c4e1164
Xyber Pastoril <xyber.pastoril@proton.me>
2023-02-24 16:21:42 +0800

    Reroute students > enrolled students and enhance student import (#10)
    Automatically enroll student based on its semester, and automatically block if there is an existing id_number.

--- app/Imports/StudentsImport.php
+++ app/Imports/StudentsImport.php
@@ -2,6 +2,7 @@
 
 namespace App\Imports;
 
+use App\Models\EnrolledStudent;
 use App\Models\Student;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
@@ -15,12 +16,24 @@ class StudentsImport implements ToModel, WithHeadingRow
     */
     public function model(array $row)
     {
-        return new Student([
-            'id_number' => $row['id_number'],
-            'last_name' => $row['last_name'],
-            'first_name' => $row['first_name'],
+        $student = Student::whereEncrypted('id_number', $row['id_number'])->first();
+
+        if(!$student) {
+            $student = Student::create([
+                'id_number' => $row['id_number'],
+                'last_name' => $row['last_name'],
+                'first_name' => $row['first_name'],
+            ]);
+        }
+
+        EnrolledStudent::firstOrCreate([
+            'student_id' => $student->id,
+            'semester_id' => 1,
+        ], [
             'degree_program_id' => $row['degree_program_id'],
             'year_level' => $row['year_level'],
         ]);
+
+        return null;
     }
 }
